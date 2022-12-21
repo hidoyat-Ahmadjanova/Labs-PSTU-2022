5821d6c763d41aac2e4b5469e6327124e44337eb
Hidoyat <118998775+hidoyat-Ahmadjanova@users.noreply.github.com>
2022-12-21 18:36:58 +0300

    Add files via upload

---
+++ алгоритм/2.1/2.1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    int n; int sum = 0;
+
+ cin >> n;
+
+    if (n <= 0)
+    {
+ cout << "Невозможно посчитать сумму натуральных чисел при n <= 0" << endl;
+    }
+    else
+    {
+        for (int i = 1; i <= n; i++)
+        {
+ sum += i;
+        }
+ cout << sum << endl;
+    }
+}
